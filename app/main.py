4dd959f2691cd2ac75da2dc17cf409846fa109ec
Steven <steven928@hotmail.fr>
2021-01-09 21:17:54 +0000

    Added timelapse path

--- app/main.py
+++ app/main.py
@@ -25,3 +25,8 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.route("/starttimelapse")
+def start_timelapse(t: int = 30000, tl: int = 2000):
+	subprocess.run("raspistill -t".split()+str(t)+" -tl "+str(tl)+" -o image%04d.jpg".split())
+	return "Démarrage du timelapse : une photo toutes les "+str(tl)+"s pendant "+str(t)+"s"
