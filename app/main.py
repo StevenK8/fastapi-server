faea53a4dea80717f405e847856df8c9c1fb4a86
Steven <steven928@hotmail.fr>
2021-01-28 00:52:52 +0100

    update main : timelapse fonctionnel

--- app/main.py
+++ app/main.py
@@ -1,12 +1,25 @@
 from typing import Optional
-
+from datetime import date
 from fastapi import FastAPI
 from pydantic import BaseModel
 import subprocess
+import sys
+import os
+import time
+import logging
+import picamera
+from datetime import datetime
+from time import sleep
 
 app = FastAPI()
 
-proc = subprocess
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
+now = datetime.now()
+
+__output_folder_name__= '/home/pi/camera/' + now.strftime("%d-%m-%Y") + '/'
+
+__default_rotation__ = 0
 
 
 class Item(BaseModel):
@@ -14,6 +27,9 @@ class Item(BaseModel):
     price: float
     is_offer: Optional[bool] = None
 
+#  Fonctions
+
+# Fonctions associées à FastAPI
 
 @app.get("/")
 def read_root():
@@ -30,17 +46,27 @@ def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
 
 
-@app.route("/timelapse/start/")
-def start_timelapse(t: int = 30000, tl: int = 2000):
-    cmd = "raspistill -t"+str(t)+"-tl"+str(tl)+"-o image%04d.jpg"
-    proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-    return "Démarrage du timelapse : une photo toutes les "+str(tl)+"s pendant "+str(t)+"s"
+@app.get("/timelapse/start/")
+def start_timelapse(length_in_seconds: int = 16, interval_in_seconds: int = 2, rotation: int = 0, iso : int = 0, shutter_speed : int = 0, autoWhiteBalance : bool = True):
+    start_time = time.time()
+    if not os.path.exists(__output_folder_name__):
+        os.makedirs(__output_folder_name__)
+    print(__output_folder_name__)
+
+    # Take pictures
+    logging.info('Opening camera...')
+    capture_images(length_in_seconds, interval_in_seconds, rotation, iso, shutter_speed, autoWhiteBalance)
+    # logging.info('Writing timestamps...')
+
+    # # Write timestamps to images
+    # write_timestamps(72,32,__output_folder_name__)
+    # logging.info('Captured images {} seconds to run'.format(str(time.time() - start_time)))
+
+    return "Timelapse terminé"
 
 
 @app.get("/timelapse/status/")
 def is_timelapse():
-    # cmd = "ps aux | grep raspistill | wc -l"
-
     # process = subprocess.Popen(cmd.split(),
     #                            stdout=subprocess.PIPE,
     #                            stderr=subprocess.STDOUT)
@@ -52,3 +78,32 @@ def is_timelapse():
 @app.get("/timelapse/stop/")
 def stop_timelapse():
     proc.kill()
+
+
+def camera_options(camera, iso, rotation, shutter_speed, autoWhiteBalance):
+    camera.iso = iso
+    camera.rotation = rotation
+    camera.shutter_speed = shutter_speed
+
+    if(shutter_speed != 0):
+        # Sleep to allow the shutter speed to take effect correctly.
+        sleep(1)
+        camera.exposure_mode = 'off'
+
+    if(not autoWhiteBalance):
+        camera.awb_mode = 'off'
+
+    return camera
+
+def capture_images(length_in_seconds, interval_in_seconds, rotation, iso, shutter_speed, autoWhiteBalance):
+    count = length_in_seconds / interval_in_seconds
+    logging.info('Taking {} shots...'.format(count))
+    with picamera.PiCamera() as camera:
+        camera.start_preview()
+        camera_options(camera, iso, rotation, shutter_speed, autoWhiteBalance)
+        time.sleep(2)
+        for filename in camera.capture_continuous(__output_folder_name__+'/img{counter:06d}.jpg'):
+            time.sleep(interval_in_seconds) # wait <interval_in_seconds> seconds
+            count -= 1
+            if count <= 0:
+                break
